dc6b348d40a840fed34fdac47ded84eef38d2eeb
tarcio teixeia <tarciogbteixeira@gmail.com>
2024-01-24 16:04:15 -0200

    desenvolvendo lógico

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import Calculator from './components/Calculator/index'
-import Result from './components/Resultado/index'
 import './Main.css'
 
 function App() {
@@ -8,7 +7,6 @@ function App() {
       <img src="./src/assets/personal_traine.svg" alt="personal treiner"></img>
       <div className="calculator">
       <Calculator/>
-      <Result/>
       </div>
     </div>
   )
--- src/components/Calculator/index.jsx
+++ src/components/Calculator/index.jsx
@@ -1,18 +1,29 @@
+import { useState } from 'react';
 import './calculator.css'
 
 
 /*Esse arquivo irá conter o container com os inputs para entrada de valores*/
 
 const Calculator = () => {
+
+    const [peso, setPeso] = useState(0);
+    const [altura, setAltura] = useState(0);
+
+const calculaImc = (weigth, heigth) => {
+    let alturaImc = heigth * heigth
+    let imc = weigth / alturaImc
+    return imc.toFixed(1)
+}
+
     return(
         <>
-        <h1 className="title">CALCULADORA - IMC </h1>
+        <h1 className="title">CALCULADORA - IMC</h1>
             <form className="calc">
                 <div className="input-box">
                     <label for="weigth">Peso em kg</label>
                     <div className="input-field">
                         <i className="fa-solid fa-weight-hanging"></i>
-                        <input type="number" id="weigth" required></input>
+                        <input type="number" id="weigth" required onChange={e => setPeso(Number(e.target.value))}/>
                         <span>kg</span>
                     </div>
                 </div>
@@ -20,11 +31,21 @@ const Calculator = () => {
                     <label for="heigth">Altura em metros</label>
                     <div className="input-field">
                         <i className="fa-solid fa-ruler"></i>
-                        <input type="number" id="heigth" required></input>
+                        <input type="number" id="heigth" required onChange={e => setAltura(Number(e.target.value))}/>
                         <span>M</span>
                     </div>
                 </div>
             </form>
+                <div className="resultado">
+                    <p>Seu IMC:</p>
+                    <h2 id='imc'>{calculaImc(peso, altura)}</h2>
+                    <p id='descricao'>Normal</p>
+                </div>
+                <div className="alerta">
+                    <i class="fa-solid fa-triangle-exclamation"></i>
+                    <p>Sua SAÚDE precisa de maior assitência, faça uma avaliação fisíca presencial agora. Clique no link.</p>
+                    <a href=""><i class="fa-solid fa-arrow-up-right-from-square"></i> Mais informações</a>
+                </div>
             </>
     )
 }
