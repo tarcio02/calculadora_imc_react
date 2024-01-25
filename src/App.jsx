0cfe907c7a3777c7f922596542058384b5d6b93e
tarcio teixeia <tarciogbteixeira@gmail.com>
2024-01-25 16:19:52 -0200

    armengo v1.1

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import './Main.css'
 function App() {
   return (
     <div className='container'>
-      <img src="./src/assets/personal_traine.svg" alt="personal treiner"></img>
+      <img src="https://raw.githubusercontent.com/tarcio02/calculadora_imc_react/641287b4d0258eee6d85c7b86256b6eb287801ad/src/assets/personal_traine.svg" alt="personal treiner"></img>
       <div className="calculator">
       <Calculator/>
       </div>
