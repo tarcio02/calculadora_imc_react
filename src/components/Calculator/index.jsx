409ea919555906ed91f36944f0b19345d0917e91
tarcio teixeia <tarciogbteixeira@gmail.com>
2024-01-24 23:05:37 -0200

    mais um pouco de lógica e estilos

--- src/components/Calculator/index.jsx
+++ src/components/Calculator/index.jsx
@@ -9,12 +9,32 @@ const Calculator = () => {
     const [peso, setPeso] = useState(0);
     const [altura, setAltura] = useState(0);
 
+    const renderizaCta = (weight, heigth)=> {
+        if(weight & heigth != "") {
+            return <>
+                
+            </>
+        }
+    }
+
 const calculaImc = (weigth, heigth) => {
     let alturaImc = heigth * heigth
-    let imc = weigth / alturaImc
-    return imc.toFixed(1)
+    let imc = (weigth / alturaImc).toFixed(1)
+
+    if(weigth == "" || imc == Infinity){
+        return <>
+        <i id='imcVazio' class="fa-solid fa-triangle-exclamation"></i>
+        <h2 id='imcVazio'>Insira os valores acima.</h2>
+        </>
+    }
+    if(imc < 17) {
+        return <>
+        <h2 id='imc'>{imc}</h2> <p id='descricao'>Magreza grave, muito abaixo do peso.</p>
+        </>
+    }
 }
 
+{/* <h2 id='imc'>{imc}</h2> <p id='descricao'>sua redenrização foi realizada</p> */}
     return(
         <>
         <h1 className="title">CALCULADORA - IMC</h1>
@@ -36,12 +56,11 @@ const calculaImc = (weigth, heigth) => {
                     </div>
                 </div>
             </form>
-                <div className="resultado">
-                    <p>Seu IMC:</p>
-                    <h2 id='imc'>{calculaImc(peso, altura)}</h2>
-                    <p id='descricao'>Normal</p>
-                </div>
-                <div className="alerta">
+            <div className="resultado">
+                <p>Seu IMC:</p>
+                {calculaImc(peso, altura)}
+            </div>
+            <div  className="alerta">
                     <i class="fa-solid fa-triangle-exclamation"></i>
                     <p>Sua SAÚDE precisa de maior assitência, faça uma avaliação fisíca presencial agora. Clique no link.</p>
                     <a href=""><i class="fa-solid fa-arrow-up-right-from-square"></i> Mais informações</a>
